ee660760cbf03a5ffb533b36c7df517bb016d030
Gerard-Mc <gerardmcdaid88@gmail.com>
2021-03-25 19:59:57 +0000

    Add product and categories models. Edit product fixture

--- products/models.py
+++ products/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=254)
+    friendly_name = models.CharField(max_length=254, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_friendly_name(self):
+        return self.friendly_name
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=254)
+    description = models.TextField(null=True, blank=True)
+    price = models.DecimalField(null=True, blank=True, max_digits=6, decimal_places=2)
+    category = models.ForeignKey('Category')
+    user_description = models.TextField(null=False, blank=False)
+    rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
+    size = models.IntegerField(validators=[MinValueValidator(2), MaxValueValidator(4)])
+    testimonial = models.TextField(null=True, blank=True)
+    image = models.ImageField(upload_to ='images/')
+
+    def __str__(self):
+        return self.name
+
