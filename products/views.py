19c1364e1d6270c49d0218ad0151d783a997ba62
Gerard-Mc <gerardmcdaid88@gmail.com>
2021-05-11 16:38:14 +0000

    Add sort by categories functionality to portfolio.html

--- products/views.py
+++ products/views.py
@@ -1,15 +1,22 @@
 from django.shortcuts import render
-from .models import Product
+from .models import Product, Category
 
-# Create your views here.
 
 def products(request):
-    """ View to display the products"""
+    """ View displays all products and sorts by categories """
 
     products = Product.objects.all()
+    categories = None
+
+    if request.GET:
+        if 'category' in request.GET:
+            categories = request.GET['category'].split(',')
+            products = products.filter(category__name__in=categories)
+            categories = Category.objects.filter(name__in=categories)
 
     context = {
         'products': products,
+        'current_categories': categories,
     }
 
     return render(request, 'products/portfolio.html', context)
