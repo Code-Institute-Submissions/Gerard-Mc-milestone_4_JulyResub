9bf97ae6725a01f742767b90b989a366bff39bac
Gerard-Mc <gerardmcdaid88@gmail.com>
2021-05-12 16:49:11 +0000

    Sort products by price & rating ascending & descending

--- products/urls.py
+++ products/urls.py
@@ -2,5 +2,4 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.portfolio, name='portfolio')
-]
+    path('', views.portfolio, name='portfolio')]
--- products/views.py
+++ products/views.py
@@ -1,24 +1,51 @@
 from django.shortcuts import render
 from .models import Product, Category
-from datetime import datetime
 
 
 def portfolio(request):
     """ View displays all products and sorts by categories """
+    # All products loaded when page loads.
     products = Product.objects.all()
-    categories = None
-    if request.GET:
-        if 'sort' in request.GET:
-            sort = request.GET['sort']
-            products = products.order_by(sort)
+    # categories_string is a conversion from list to string
+    # to be used in a query string for user sorting options in portfolio.html.
+    categories_string = ""
+    categories = Category.objects.values('name')
 
+    if request.GET:
         if 'category' in request.GET:
-            categories = request.GET['category'].split(',')
-            #  The split() splits the string into a list
+            # split() splits the string into a list
+            categories = request.GET['category'].split(",")
+            #  Filter products to the above category/categories.
+            products = products.filter(category__name__in=categories)
+            # Converting the list back to string
+            if len(categories) > 1:
+                for i in range(len(categories)):
+                    # Adds comma to maintain the established format.
+                    categories_string += "" + categories[i] + ","
+                # Removes comma after final word.
+                categories_string = categories_string[:-1]
+                print(categories_string)
+            else:
+                # Converts lists with a single item to string.
+                for i in categories:
+                    categories_string += i
+            # Takes sort parameter value from URL.
+            if 'sort' in request.GET:
+                sort = request.GET['sort']
+                sortkey = sort
+                # Checks the direction param value.
+                if 'direction' in request.GET:
+                    direction = request.GET['direction']
+                    # Inverts the sort direction.
+                    if direction == 'desc':
+                        sortkey = f'-{sortkey}'
+                        products = products.order_by(sortkey)
+                # Sort products by chosen entity field.
+                products = products.order_by(sortkey)
 
     context = {
         'products': products,
-        'current_categories': categories,
+        'category': categories_string,
     }
 
     return render(request, 'products/portfolio.html', context)
