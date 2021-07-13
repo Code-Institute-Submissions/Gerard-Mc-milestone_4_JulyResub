e1a9e3434102daff64733fe4855d6554294b567e
Gerard-Mc <gerardmcdaid88@gmail.com>
2021-07-13 14:14:48 +0000

    Add missing form from previous commit. Create checkout view

--- checkout/views.py
+++ checkout/views.py
@@ -1,7 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
+from django.contrib import messages
+
+from .forms import OrderForm
 
 
 def checkout(request):
-    """ Returns the home page """
+    cart = request.session.get('cart', {})
+    if not cart:
+        messages.error(request, "Your cart is empty.")
+        return redirect(reverse('order'))
 
-    return render(request, 'checkout/checkout.html')
+    order_form = OrderForm()
+    template = 'checkout/checkout.html'
+    context = {
+        'order_form': order_form,
+    }
+
+    return render(request, template, context)
